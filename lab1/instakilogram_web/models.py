0925384946d2ec0c4fb72508c0b86f8d8a5da257
Natalia Sobisz <nata.so@wp.pl>
2023-10-12 16:32:44 +0200

    lab01 ver2.0

--- lab1/instakilogram_web/models.py
+++ lab1/instakilogram_web/models.py
@@ -2,10 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-class Reel(models.Model):
-    title = models.CharField(max_length=150, blank=False,unique=False)
-    year = models.PositiveSmallIntegerField(default=2023)
 
-
-    def __str__(self):
-        return self.title
