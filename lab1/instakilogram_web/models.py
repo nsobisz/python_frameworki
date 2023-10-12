8b4efe8abbdc17f24c5d35b5dd5ed7b52216c33e
Natalia Sobisz <nata.so@wp.pl>
2023-10-12 17:05:31 +0200

    lab01 ver2.0

--- lab1/instakilogram_web/models.py
+++ lab1/instakilogram_web/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Post (models.Model):
+    title = models.CharField(max_length=64, blank=False)
+    description = models.TextField(max_length=1000, default="")
+    year = models.PositiveSmallIntegerField(default=2023)
+    upload = models.FileField(upload_to="uploads/", default="")
+    def __str__(self):
+        return self.title
+
 
