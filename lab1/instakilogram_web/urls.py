322727d9bf72d3b8e7f72cb32a4bf89f378a3f47
Natalia Sobisz <nata.so@wp.pl>
2023-11-08 20:31:41 +0100

    refactor

--- lab1/instakilogram_web/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from instakilogram_web.views import test_response, user_profile
-
-urlpatterns = [
-    path('test/', test_response),
-    path('profile/<username>/', user_profile, name='user_profile'),
-]
